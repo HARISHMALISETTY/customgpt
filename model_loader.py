03bb7dfb9d2ac7a53d8ee043dcdbfc5df473ee51
HARISHMALISETTY <malisettyharish@gmail.com>
2025-12-10 19:06:53 +0530

    model changed

--- model_loader.py
+++ model_loader.py
@@ -2,15 +2,13 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
 def load_model():
-    MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+    MODEL_NAME = "distilgpt2"   # Best model for HF free tier
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-    # CPU-only safe loading for HuggingFace Spaces
     model = AutoModelForCausalLM.from_pretrained(
         MODEL_NAME,
-        torch_dtype=torch.float32,    # MUST use float32 on CPU
-        low_cpu_mem_usage=True
+        torch_dtype=torch.float32   # required for CPU inference
     )
 
     device = torch.device("cpu")
